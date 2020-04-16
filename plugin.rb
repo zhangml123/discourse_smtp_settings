0bbb55a16e0efae7ad01f04a85c6257b1826d696
zhangml123 <s15532292192@163.com>
2020-04-17 00:11:42 +0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -5,9 +5,12 @@
 # version: 1.0
 # authors: 
 # url: https://github.com/zhangml123/discourse_smtp_settings
+Rails.configuration.action_mailer.smtp_settings={ 
+	      port: 21
+	    }
 
-after_initialize do
-	Discourse::Application.configure do
-		config.action_mailer.smtp_settings={}
-	end
+Discourse::Application.configure do
+	config.action_mailer.smtp_settings={ 
+      port: 22
+    }
 end
