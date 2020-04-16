e4345fcc817c10d20d33e25bdf540b18401389c5
zhangml123 <s15532292192@163.com>
2020-04-16 23:45:15 +0800

    Create plugin.rb

---
+++ plugin.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# name: discourse_smtp_settings
+# about: discourse smtp settings
+# version: 1.0
+# authors: 
+# url: https://github.com/zhangml123/discourse_smtp_settings
+
+after_initialize do
+	Discourse::Application.configure do
+		config.action_mailer.smtp_settings=[]
+	end
+end
